11bd803ecf96895d8f750d364ad708f32fc83e15
unknown <shubham22mohod@gmail.com>
2019-03-21 09:47:41 +0530

    by me

---
+++ firstapp/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Greet from "./components/Greet"
+import Welcome from "./components/Welcome"
+import Message from "./components/Message"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <Greet/>
+        <Welcome name="shubham"/> */}
+        <Message/>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ firstapp/src/components/Message.js
@@ -0,0 +1,28 @@
+import React,{ Component } from 'react'
+class Message extends Component {
+
+    constructor(){
+        super();
+        this.state={
+            message:'Welcome visistor'
+        }
+    }
+
+    changeMessage(){
+            this.setState({
+                message:'thsnka for s'
+            })
+    }
+
+
+    render() {
+        return (
+            <div>
+            <h1>Welcome {this.state.message}</h1>
+            <button onClick={()=>this.changeMessage()}>Subscribe</button>
+            </div>
+        );
+    }
+}
+
+export default Message;
